0e2310d03e34c836063d39539019c5fab38cded1
Darlley <systembrtt@gmail.com>
2021-08-16 11:48:07 -0400

    inicio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CollabCode
+
+Resolução de exercícios dos cursos da CollabCode
+
+![Logotipo](https://collabcode.training/static/media/logo-avatar.3b0b3a3d.svg)
---
+++ matching-game/main.js
@@ -0,0 +1,3 @@
+// Toda variavel com $ indica que ela acessa algum elemento da DOM
+const $root = document.querySelector('#root');
+$root.textContent = `Darlley`;
