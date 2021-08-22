f60a5be1444a0efa704a1a9a8f3441e0cb153be8
Darlley <systembrtt@gmail.com>
2021-08-22 19:37:21 -0400

    :sparkles: Container CardGame
    Container CardsGame

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,11 +1,13 @@
-import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
+import './src/styles/settings/colors.css'
 import './src/styles/elements/base.css'
 
 // Toda variavel com $ indica que ela acessa algum elemento da DOM
 import CardGame from './src/components/CardGame/';
 
-const $root = document.querySelector('#root');
+const $root = document.querySelector('[data-js="app"]');
 const $htmlCardGame = CardGame();
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+for(let i=0; i<6; i++){
+    $root.insertAdjacentHTML("beforeend", $htmlCardGame);
+}
