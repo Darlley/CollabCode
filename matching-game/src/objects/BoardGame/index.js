fe77bcea60c96da7f5e74d3ea9c85c1361bdcad6
Darlley <systembrtt@gmail.com>
2021-09-01 20:34:14 -0400

    :construction: Finish the BoardGame object

--- matching-game/src/objects/BoardGame/index.js
+++ matching-game/src/objects/BoardGame/index.js
@@ -1,7 +1,12 @@
+import './style.css'
 import CardGame from '../../components/CardGame'
 
 function BoardGame(amountCards) {
-    const $htmlBoardGame = CardGame().repeat(amountCards);
-    return $htmlBoardGame;
+    const $htmlContent = CardGame().repeat(amountCards);
+    return `
+        <section class="board-game">
+            ${$htmlContent}
+        </section>
+    `;
 }
 export default BoardGame;
