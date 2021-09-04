48db773c0e3322b5a92097d1e505a6f6134a996b
Darlley <systembrtt@gmail.com>
2021-09-03 20:01:43 -0400

    :sparkles: Add the BoardScore in main

--- matching-game/main.js
+++ matching-game/main.js
@@ -5,10 +5,10 @@ import './src/styles/elements/base.css'
 // Toda variavel com $ indica que ela acessa algum elemento da DOM
 import BoardGame from './src/objects/BoardGame';
 import PlayerName from './src/components/PlayerName';
+import BoardScore from './src/objects/BoardScore';
 
 const $root = document.querySelector('#root');
 $root.insertAdjacentHTML("beforeend", `
-    ${PlayerName('Player1')}
-    ${PlayerName('Player2')}
+    ${BoardScore()}
     ${BoardGame(6)}
 `);
